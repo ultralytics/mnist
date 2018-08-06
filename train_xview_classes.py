705bfebfdb02e94aa084e48b878c31501301521a
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-06 23:24:32 +0200

    updates

--- train_xview_classes.py
+++ train_xview_classes.py
@@ -132,6 +132,7 @@ def main(model):
     np.random.seed(0)
     torch.manual_seed(0)
     if cuda:
+        torch.cuda.empty_cache()
         torch.cuda.manual_seed(0)
         torch.cuda.manual_seed_all(0)
         torch.backends.cudnn.benchmark = True
