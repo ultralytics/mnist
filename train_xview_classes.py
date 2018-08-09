57f4b7be6ed6c06405b182b0f77ae69e5b5ddbdd
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-10 01:51:59 +0200

    updates

--- train_xview_classes.py
+++ train_xview_classes.py
@@ -39,9 +39,9 @@ def xview_class_weights(indices):  # weights of each class in the training set, 
 class ConvNetb(nn.Module):
     def __init__(self, num_classes=60):
         super(ConvNetb, self).__init__()
-        n = 64  # initial convolution size
+        n = 50  # initial convolution size
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, n, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, n, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(n),
             nn.LeakyReLU())
         self.layer2 = nn.Sequential(
@@ -65,9 +65,33 @@ class ConvNetb(nn.Module):
         self.fully_conv = nn.Conv2d(n * 16, num_classes, kernel_size=4, stride=1, padding=0, bias=True)  # 5 layer s1
 
     def forward(self, x):  # 500 x 1 x 64 x 64
-        #print(x.shape)
-        x = self.layer1(x)
-        #print(x.shape)
+        # print(x.shape)
+
+        xa = [x.clone()]  # 0 rot
+        xa.append(x.clone().flip(2).flip(3))  # 180 rot
+        xa.append(x.clone().transpose(2, 3).flip(2))  # -90 rot
+        xa.append(x.clone().transpose(2, 3).flip(3))  # +90 rot
+
+        # import matplotlib.pyplot as plt
+        # plt.subplot(2, 2, 1).imshow(a0[1, 1].detach().numpy())
+        # plt.subplot(2, 2, 2).imshow(a1[1, 1].detach().numpy())
+        # plt.subplot(2, 2, 3).imshow(a2[1, 1].detach().numpy())
+        # plt.subplot(2, 2, 4).imshow(a3[1, 1].detach().numpy())
+
+        v = []
+        b = []
+        for i, a in enumerate(xa):
+            b.append(self.layer1(a))
+            v.append(b[i].sum(3).sum(2).sum(1).unsqueeze(1))
+        v = torch.cat(v, 1)
+        best_transform_index = torch.argmax(v, 1)
+
+        for i, bt in enumerate(best_transform_index):
+            print(i, bt)
+            b[0][i] = b[bt][i]
+        x = b[0]
+
+        # print(x.shape)
         x = self.layer2(x)
         # print(x.shape)
         x = self.layer3(x)
@@ -252,7 +276,6 @@ def main(model):
 
             x -= rgb_mean
             x /= rgb_std
-            x = x[:, 0] / 3 + x[:, 1] / 3 + x[:, 2] / 3
 
             yhat = model(x)
             loss = criteria(yhat, y)
@@ -291,7 +314,6 @@ def main(model):
 
             x -= rgb_mean
             x /= rgb_std
-            x = x[:, 0] / 3 + x[:, 1] / 3 + x[:, 2] / 3
 
             with torch.no_grad():
                 yhat = model(x)
