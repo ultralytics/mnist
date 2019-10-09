bbd035f14a6dd7e4fd7acedc46015cd51abaf6b3
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-10-09 20:23:49 +0200

    updates

--- train_resnet.py
+++ train_resnet.py
@@ -9,12 +9,12 @@ from utils.utils import *
 
 
 def main(model):
-    lr = 0.1
+    lr = 0.01
     epochs = 1000
     printerval = 1
     patience = 100
-    batch_size = 128
-    device = torch_utils.select_device(device='1')
+    batch_size = 32
+    device = torch_utils.select_device(device='1' if torch.cuda.is_available() else '')
     torch_utils.init_seeds()
 
     # MNIST Dataset
@@ -33,8 +33,8 @@ def main(model):
 
     x, y = [], []
     for i, c in enumerate(d):
-        for file in tqdm(glob.glob('%s/*.*' % c)):
-            img = cv2.resize(cv2.imread(file), (224, 224))  # BGR
+        for file in tqdm(glob.glob('%s/*.*' % c)[:1000]):
+            img = cv2.resize(cv2.imread(file), (64, 64))  # BGR
             img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
             img = np.expand_dims(img, axis=0)  # add batch dim
             img = np.ascontiguousarray(img, dtype=np.float32)  # uint8 to float32
@@ -43,17 +43,24 @@ def main(model):
             x.append(img)  # input
             y.append(i)  # output
 
+    print('Concatenating...')
     x = np.concatenate(x, 0)
     y = np.array(y)
+    nc = len(np.unique(y))  # number of classes
 
+    print('Shuffling...')
     shuffle_data(x, y)
+
+    print('Splitting into train and validate sets...')
     x, y, xtest, ytest, *_ = split_data(x, y, train=0.9, validate=0.10, test=0.0)
 
+    print('Creating Train Dataloader...')
     train_loader2 = create_batches(x=torch.Tensor(x),  # [60000, 1, 28, 28]
                                    y=torch.Tensor(y).squeeze().long(),  # [60000]
                                    batch_size=batch_size, shuffle=True)
     del x, y
 
+    print('Creating Test Dataloader...')
     test_loader2 = create_batches(x=torch.Tensor(xtest),
                                   y=torch.Tensor(ytest).squeeze().long(),
                                   batch_size=batch_size)
@@ -78,7 +85,7 @@ def main(model):
     #                               batch_size=batch_size)
 
     model = model.to(device)
-    criteria1 = nn.CrossEntropyLoss()
+    #criteria1 = nn.CrossEntropyLoss()
     criteria2 = nn.BCEWithLogitsLoss()
 
     # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -87,17 +94,19 @@ def main(model):
 
     print('Starting training...')
 
+    def class2binary(x, c):
+        b = torch.zeros_like(x)
+        b[range(len(x)), c] = 1.0
+        return b
+
     def train(model):
-        for i, (x, y) in enumerate(train_loader2):
+        for i, (x, y) in tqdm(enumerate(train_loader2), desc='Training', total=len(train_loader2)):
             x, y = x.to(device), y.to(device)
             # x = x.repeat([1, 3, 1, 1])  # grey to rgb
             # x /= 255.  # rescale to 0-1
 
             pred = model(x)
-
-            y2 = torch.zeros_like(pred)
-            y2[range(len(pred)), y] = 1.0
-            loss = criteria2(pred, y2)
+            loss = criteria2(pred, class2binary(pred, y))
 
             optimizer.zero_grad()
 
@@ -106,17 +115,17 @@ def main(model):
 
     def test(model):
         # x, y = test_data
-        for i, (x, y) in enumerate(test_loader2):
+        for i, (x, y) in tqdm(enumerate(test_loader2), desc='Testing', total=len(test_loader2)):
             x, y = x.to(device), y.to(device)
             # x = x.repeat([1, 3, 1, 1])  # grey to rgb
             # x /= 255.  # rescale to 0-1
 
             yhat = model(x)
-            loss = criteria1(yhat, y)
+            loss = criteria2(yhat, class2binary(yhat, y))
             yhat_number = torch.argmax(yhat.data, 1)
 
             accuracy = []
-            for j in range(10):
+            for j in range(nc):
                 j = y == j
                 accuracy.append((yhat_number[j] == y[j]).float().mean() * 100.0)
 
